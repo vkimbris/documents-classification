ab3a89d13b72b49889f1f51c8479b53782641f62
Ilia Petrov <ilia.petrov@netcracker.com>
2024-04-13 15:58:39 +0300

    Added parse function and tika interaction

--- app/src/document_parser.py
+++ app/src/document_parser.py
@@ -1,12 +1,15 @@
 from typing import Any
+from tika import parser
+
+TIKA_SERVER = "tika:9998"
 
 
-def parse_document(document: Any) -> str:
+def parse_document(buffer_document: Any) -> str:
     """
     Returns content of the input document.
 
     document : Any
         document is a file in any format (e.g. .doc, .docx, .pdf, ...)
     """
-    
-    raise NotImplementedError
+    document_data = parser.from_buffer(buffer_document, f"http://{TIKA_SERVER}/tika")
+    raise document_data['content']
