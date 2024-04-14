ab2bc6d19f731a3e6fb5ec6652410187db03ed12
Ilia Petrov <ilia.petrov@netcracker.com>
2024-04-14 07:13:06 +0300

    wide mode

--- frontend/app.py
+++ frontend/app.py
@@ -5,6 +5,7 @@ from src.renders import render_classify_section, render_thematic_section, \
 
 
 def main():
+    st.set_page_config(layout="wide")
     st.sidebar.title("Разделы")
     page = st.sidebar.radio("Выбрать раздел:", ["Классификация документов",
                                                  "Обучение классификатора",
@@ -28,7 +29,6 @@ def main():
 st.session_state['uploaded_files'] = []
 st.session_state['training_file'] = None
 st.session_state['ner_file'] = None
-
 
 if __name__ == "__main__":
     main()
